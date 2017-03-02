a4afbff7ab5d60368d28d65ffddba04fed9cea1d
j.borbely <joe.borbely@gmail.com>
2017-03-02 17:30:53 +1300

    v1.0.0 release

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,6 +2,15 @@
 Release Notes
 =============
 
+Version 1.0.0 (2017.03.02)
+==========================
+- separate **install**, **uninstall**, **create** and **list** functions into different modules
+- fix MSL namespace
+- edit test_envs.py to work with colorama and update stdout in real time
+- add **--yes** and **--release-info** flags for CLI
+- create documentation and unit tests
+- many bug fixes
+
 Version 0.1.0 (2017.02.19)
 ==========================
 - Initial release
--- msl/package_manager/__init__.py
+++ msl/package_manager/__init__.py
@@ -1,18 +1,20 @@
 """
-Allows you to install, uninstall, list and create MSL packages.
+The MSL-Package-Manager allows you to install, uninstall, list and create MSL packages.
+
+The following constants are available.
 """
 import sys
 from collections import namedtuple
 
 __author__ = 'Joseph Borbely'
 __copyright__ = '\xa9 2017, ' + __author__
-__version__ = '0.1.0'
+__version__ = '1.0.0'
 
 version_info = namedtuple('version_info', 'major minor micro')(*map(int, __version__.split('.')))
 """:py:func:`~collections.namedtuple`: Contains the version information as a (major, minor, micro) tuple."""
 
 PKG_NAME = 'msl-package-manager'
-""":py:class:`str`: The name of the package when it is installed"""
+""":py:class:`str`: The name of this package when it is installed"""
 
 IS_PYTHON2 = sys.version_info[0] == 2
 """:py:class:`bool`: Whether Python 2.x is being used."""
@@ -24,7 +26,7 @@ from .helper import *
 from .create import create
 from .install import install
 from .uninstall import uninstall
-from .list import show
+from .print_list import print_list
 
 import colorama
 colorama.init(autoreset=True)
