b7fed93a5b450e4c419e1ddf455adae277bb303e
j.borbely <joe.borbely@gmail.com>
2017-08-30 11:54:39 +1200

    re-create the __init__.py files after a msl package is uninstalled

--- msl/package_manager/uninstall.py
+++ msl/package_manager/uninstall.py
@@ -1,6 +1,7 @@
 """
 Use pip to uninstall MSL packages.
 """
+import os
 import sys
 import subprocess
 
@@ -19,5 +20,19 @@ def uninstall(names='ALL', yes=False):
     yes : :obj:`bool`, optional
         Don't ask for confirmation to uninstall. Default is to ask before uninstalling.
     """
+
+    # after a MSL package gets uninstalled we must re-create the two "__init__.py"
+    # package files to maintain the 'msl' namespace
+    template_dir = os.path.join(os.path.dirname(__file__), 'template', 'msl')
+    with open(os.path.join(template_dir, '__init__.py.template'), 'rt') as fp:
+        msl_init = fp.readlines()
+    with open(os.path.join(template_dir, 'examples', '__init__.py.template'), 'rt') as fp:
+        msl_examples_init = fp.readlines()
+
     for pkg in _get_packages('uninstall', names, yes):
         subprocess.call([sys.executable, '-m', 'pip', 'uninstall', '-y', pkg])
+
+        with open(os.path.join(os.path.dirname(__file__), '..', '__init__.py'), 'wt') as fp:
+            fp.writelines(msl_init)
+        with open(os.path.join(os.path.dirname(__file__), '..', 'examples', '__init__.py'), 'wt') as fp:
+            fp.writelines(msl_examples_init)
