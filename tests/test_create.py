655020d7382e22b585cf29f6ad75b321d625a309
jborbely <joe.borbely@gmail.com>
2018-02-15 22:26:14 +1300

    change tempfile.tempdir to tempfile.gettempdir()

--- tests/test_create.py
+++ tests/test_create.py
@@ -9,7 +9,7 @@ import msl.package_manager as pm
 def test_create():
     filename = 'akjdSKmkmklmKMgvrd4ESExKOKuh'
     pm.create(names=filename, author='Joe', email='a.b@c.com', path=tempfile.gettempdir())
-    path = os.path.join(tempfile.tempdir, 'msl-'+filename)
+    path = os.path.join(tempfile.gettempdir(), 'msl-'+filename)
     assert os.path.isdir(path)
     shutil.rmtree(path)
 
