d8bac3079e37628ae5e2bfcf0e94a36c3ed2e26e
jborbely <joe.borbely@gmail.com>
2018-10-26 12:59:08 +1300

    use a raw string for the __version__ re.search

--- msl/package_manager/__init__.py
+++ msl/package_manager/__init__.py
@@ -10,7 +10,7 @@ __author__ = 'Joseph Borbely'
 __copyright__ = '\xa9 2017 - 2018, ' + __author__
 __version__ = '2.2.0.dev0'
 
-_v = re.search('(\d+)\.(\d+)\.(\d+)[.-]?(.*)', __version__).groups()
+_v = re.search(r'(\d+)\.(\d+)\.(\d+)[.-]?(.*)', __version__).groups()
 
 version_info = namedtuple('version_info', 'major minor micro releaselevel')(int(_v[0]), int(_v[1]), int(_v[2]), _v[3])
 """:obj:`~collections.namedtuple`: Contains the version information as a (major, minor, micro, releaselevel) tuple."""
