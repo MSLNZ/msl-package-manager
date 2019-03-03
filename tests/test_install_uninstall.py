5a5f2d8f45ea21946f3c29f8c3c6a9423476e6f7
jborbely <joe.borbely@gmail.com>
2019-03-03 22:03:24 +1300

    uninstall msl-io if previously installed

--- tests/test_install_uninstall.py
+++ tests/test_install_uninstall.py
@@ -5,9 +5,12 @@ def test_install_then_uninstall():
 
     # make sure that MSL-IO is not installed
     pkgs = utils.installed()
+    if 'msl-io' in pkgs:
+        uninstall('io', yes=True)
+    pkgs = utils.installed()
     assert 'msl-io' not in pkgs
 
-    # install from PyPI
+    # install MSL-IO
     install('io', yes=True)
 
     # make sure that MSL-IO is installed
