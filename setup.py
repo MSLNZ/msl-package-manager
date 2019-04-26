f82e71d1488df1390d38bfbd0b4d01c6d4ce593e
jborbely <joe.borbely@gmail.com>
2019-04-26 13:32:51 +1200

    change author and author_email in setup.py

--- setup.py
+++ setup.py
@@ -96,8 +96,8 @@ sphinx = ['sphinx', 'sphinx_rtd_theme'] if needs_sphinx else []
 setup(
     name=fetch_init('_PKG_NAME'),
     version=fetch_init('__version__'),
-    author=fetch_init('__author__'),
-    author_email='joseph.borbely@measurement.govt.nz',
+    author='Measurement Standards Laboratory of New Zealand',
+    author_email='info@measurement.govt.nz',
     url='https://github.com/MSLNZ/' + fetch_init('_PKG_NAME'),
     description='Install, uninstall, update, list and create MSL packages',
     long_description=read('README.rst'),
