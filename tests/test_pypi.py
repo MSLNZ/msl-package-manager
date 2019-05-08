3a63b56ba6093c6fd5e66c1eb1d95c33580e4727
jborbely <joe.borbely@gmail.com>
2019-05-09 10:05:03 +1200

    update test_pypi() to include GTC

--- tests/test_pypi.py
+++ tests/test_pypi.py
@@ -3,7 +3,8 @@ import msl.package_manager as pm
 
 def test_pypi():
     pkgs = pm.pypi()
-    assert len(pkgs) == 3
+    assert len(pkgs) == 4
     assert pm._PKG_NAME in pkgs
     assert 'msl-loadlib' in pkgs
     assert 'msl-network' in pkgs
+    assert 'GTC' in pkgs
