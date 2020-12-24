a39d9cdc0b20025b28277ae6300f1745fc37c182
jborbely <joe.borbely@gmail.com>
2020-12-25 12:33:36 +1300

    add support for Python 3.9

--- setup.py
+++ setup.py
@@ -170,6 +170,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     setup_requires=sphinx + pytest_runner,
     tests_require=tests_require,
