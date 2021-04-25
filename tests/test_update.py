64357845d9c60e7cb42a3085dd14644c254d1a68
jborbely <joe.borbely@gmail.com>
2021-04-26 09:35:14 +1200

    pop pytest packages and create teardown_module function

--- tests/test_update.py
+++ tests/test_update.py
@@ -1,4 +1,7 @@
+import sys
 import importlib
+
+import pytest
 
 from msl.package_manager import (
     install,
@@ -9,12 +12,24 @@ from msl.package_manager import (
 )
 
 
-def test_update_msl_and_non_msl():
+def cleanup():
     msl_installed = installed()
     if 'msl-loadlib' in msl_installed:
         uninstall('loadlib', yes=True)
     if 'msl-io' in msl_installed:
         uninstall('io', yes=True)
+
+
+def teardown_module():
+    cleanup()
+
+
+@pytest.mark.skipif(
+    sys.version_info[:2] == (2, 7) and sys.platform == 'darwin',
+    reason='hyphen gets appended to msl-loadlib version (0.7.0-)'
+)
+def test_update_msl_and_non_msl():
+    cleanup()
 
     # msl-io has xlrd<2.0 as a dependency
     # make sure that xlrd is not >2.0 after updating
@@ -30,6 +45,9 @@ def test_update_msl_and_non_msl():
     assert 'msl-io' not in outdated
 
     assert installed()['msl-loadlib']['version'] == '0.7.0'
+
+    outdated.pop('pytest', None)
+    outdated.pop('pytest-cov', None)
 
     update('loadlib==0.8.0', include_non_msl=True, yes=True)
 
