6ef16c560349676c70f890cff4437c0ec6a84eb5
jborbely <joe.borbely@gmail.com>
2021-05-17 11:46:24 +1200

    use USERPROFILE when downloading artifacts on Windows

--- conftest.py
+++ conftest.py
@@ -8,27 +8,30 @@ from msl.package_manager import (
     utils,
 )
 
-stream = io.BytesIO()
-handler = logging.StreamHandler(stream=stream)
+print('HOME_DIR: {}'.format(utils._HOME_DIR))
+print('AUTH_PATH: {}'.format(utils._GITHUB_AUTH_PATH))
 
-orig_level = int(utils.log.level)
-utils.set_log_level(logging.ERROR)
-utils.log.addHandler(handler)
-
-if not pypi():
-    pypi(update_cache=True)
-
-value = stream.getvalue()
-if value:
-    sys.exit('Cannot update PyPI cache\n{}'.format(value))
-
-if not github():
-    github(update_cache=True)
-
-value = stream.getvalue()
-if value:
-    sys.exit('Cannot update GitHub cache\n{}'.format(value))
-
-utils.log.removeHandler(handler)
-utils.set_log_level(orig_level)
-stream.close()
+# stream = io.BytesIO()
+# handler = logging.StreamHandler(stream=stream)
+#
+# orig_level = int(utils.log.level)
+# utils.set_log_level(logging.ERROR)
+# utils.log.addHandler(handler)
+#
+# if not pypi():
+#     pypi(update_cache=True)
+#
+# value = stream.getvalue()
+# if value:
+#     sys.exit('Cannot update PyPI cache\n{}'.format(value))
+#
+# if not github():
+#     github(update_cache=True)
+#
+# value = stream.getvalue()
+# if value:
+#     sys.exit('Cannot update GitHub cache\n{}'.format(value))
+#
+# utils.log.removeHandler(handler)
+# utils.set_log_level(orig_level)
+# stream.close()
