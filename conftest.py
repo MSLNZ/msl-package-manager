0272ba8c8f553322f950512d43c4f1d0bd5ea3ae
jborbely <joe.borbely@gmail.com>
2021-05-16 20:46:29 +1200

    check for logging ERROR messages in conftest.py

--- conftest.py
+++ conftest.py
@@ -1,12 +1,32 @@
+import io
 import sys
+import logging
 
 from msl.package_manager import (
     pypi,
     github,
+    utils,
 )
 
-if not pypi() and not pypi(update_cache=True):
+stream = io.BytesIO()
+handler = logging.StreamHandler(stream=stream)
+
+orig_level = int(utils.log.level)
+utils.set_log_level(logging.ERROR)
+utils.log.addHandler(handler)
+
+if not pypi():
+    pypi(update_cache=True)
+
+if stream.getbuffer():
     sys.exit('Cannot update PyPI cache')
 
-if not github() and not github(update_cache=True):
+if not github():
+    github(update_cache=True)
+
+if stream.getbuffer():
     sys.exit('Cannot update GitHub cache')
+
+utils.log.removeHandler(handler)
+utils.set_log_level(orig_level)
+stream.close()
