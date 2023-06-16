cd7781095450ec3852890a1cbbefa344b81a4349
jborbely <joe.borbely@gmail.com>
2023-06-16 18:03:07 +1200

    add msl-io to test_pypi

--- tests/test_pypi.py
+++ tests/test_pypi.py
@@ -3,8 +3,9 @@ import msl.package_manager as pm
 
 def test_pypi():
     pkgs = pm.pypi()
-    assert len(pkgs) == 5
+    assert len(pkgs) == 6
     assert pm.utils._PKG_NAME in pkgs
+    assert 'msl-io' in pkgs
     assert 'msl-loadlib' in pkgs
     assert 'msl-network' in pkgs
     assert 'GTC' in pkgs
